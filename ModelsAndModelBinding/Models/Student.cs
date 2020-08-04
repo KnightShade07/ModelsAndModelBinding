75cf44a789dd0bb5d33bc70ace7e9cf3c1f55f33
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-08-04 11:36:31 -0700

    Add project files.

---
+++ ModelsAndModelBinding/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelsAndModelBinding.Models
+{
+    /// <summary>
+    /// Represents an individual student.
+    /// </summary>
+    public class Student
+    {
+        public int StudentId { get; set; }
+        /// <summary>
+        /// A student's first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The student's date of birth.
+        /// </summary>
+
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// The student's email address.
+        /// </summary>
+
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// The student's phone number!
+        /// </summary>
+
+        public string PhoneNumber { get; set; }
+    }
+}
